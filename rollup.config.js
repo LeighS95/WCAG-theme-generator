662ca392c0dafb2fcb9a3f7a6bc795d95007993d
Stephen Leigh <sleigh@uplandsoftware.com>
2022-03-01 11:41:43 +0000

    n

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,62 @@
-import babel from 'rollup-plugin-babel';
-import eslint from 'rollup-plugin-eslint';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
+import path from 'path';
+import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import strip from '@rollup/plugin-strip';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
-    entry: 'lib/index.js',
-    dest: 'dist/index.min.js',
-    format: 'iife',
-    sourceMap: 'inline',
-    plugins: [
-        resolve({
-            jsnext: true,
-            main: true,
-            browser: true
-        }),
-        commonjs(),
-        eslint({
-            exclude: 'node_modules'
-        }),
-        babel({
-            exclude: 'node_modules'
-        }),
-        replace({
-            exclude: 'node_modules',
-            ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-        })
-    ]
-}
+const devMode = (process.env.NODE_ENV === 'development');
+
+export default [
+    {
+        // UMD
+        input: 'lib/index.js',
+        output: {
+            name: 'wcag',
+            file: `dist/${pkg.name}.min.js`,
+            format: 'umd',
+            esModule: false,
+            exports: 'named',
+            sourceMap: true
+        },
+        external: [
+            "tinycolor2"
+        ],
+        plugins: [
+            resolve(),
+            babel({
+                exclude: 'node_modules/**',
+                presets: ['@babel/preset-env'],
+                babelHelpers: 'bundled'
+            }),
+            terser()
+        ]
+    },
+    {
+        // CJS/ESM
+        input: 'lib/index.js',
+        output: [
+            {
+                dir: 'dist/cjs',
+                format: 'cjs',
+                exports: 'named',
+                sourceMap: true
+            },
+            {
+                dir: 'dist/es',
+                format: 'esm',
+                exports: 'named',
+                sourceMap: true,
+                // plugins: [getBabelOutputPlugin({ preset: ['@babel/preset-env'] })]
+            }
+        ],
+        external: [
+            "tinycolor2"
+        ],
+        plugins: [
+            resolve()
+        ]
+    }
+];
